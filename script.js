526aca63d89c266adef534328d559659f46ab608
c82676b9a8 <c82676b9a8@proton.me>
2025-08-16 00:09:35 +0200

    Add test website files

---
+++ script.js
@@ -0,0 +1,4 @@
+// Change background color when the button is clicked
+document.getElementById("changeColorButton").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#ecf0f1";
+});
