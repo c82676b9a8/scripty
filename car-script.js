4873c1fe1ad46b90583893d7b3e4118d55f00c45
c82676b9a8 <c82676b9a8@proton.me>
2025-08-21 07:13:09 +0200

    Add car page

---
+++ car-script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const carImage = document.querySelector(".car-image");
+
+    carImage.addEventListener("click", () => {
+        alert("Click to learn more about the latest model!");
+    });
+});
