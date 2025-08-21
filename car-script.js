574af4f45db372d131ab94e7e717a953134a88eb
c82676b9a8 <c82676b9a8@proton.me>
2025-08-21 07:23:06 +0200

    Adxfgh

--- car-script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const carImage = document.querySelector(".car-image");
-
-    carImage.addEventListener("click", () => {
-        alert("Click to learn more about the latest model!");
-    });
-});
